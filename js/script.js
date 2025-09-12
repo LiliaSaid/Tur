1723c4065d40b61fc7477952f036fb86f2086a89
Lilia Said <yazminsaid@gmail.com>
2025-09-11 21:39:22 -0300

    se organiza el codigo, html,javasc y css

---
+++ js/script.js
@@ -0,0 +1,47 @@
+const formulario = document.getElementById('form-turno');
+  const lista = document.getElementById('lista-turnos');
+
+  formulario.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const nombre = document.getElementById('nombre').value;
+    const fecha = document.getElementById('fecha').value;
+    const hora = document.getElementById('hora').value;
+
+    const item = document.createElement('li');
+
+    // Crear span para mostrar el contenido
+    const contenido = document.createElement('span');
+    contenido.textContent = nombre + " - " + fecha + " a las " + hora;
+
+    // Crear botón de editar
+    const btnEditar = document.createElement('button');
+    btnEditar.className = 'btnEditar';
+    btnEditar.textContent = 'Modificar';
+    btnEditar.addEventListener('click', () => {
+      // Poner los datos en los campos del formulario para editar
+      document.getElementById('nombre').value = nombre;
+      document.getElementById('fecha').value = fecha;
+      document.getElementById('hora').value = hora;
+
+      // Eliminar el elemento para que se vuelva a agregar con cambios
+      lista.removeChild(item);
+    });
+
+    // Crear botón de eliminar
+    const btnEliminar = document.createElement('button');
+    btnEliminar.textContent = 'Eliminar';
+    btnEliminar.className = 'btnEliminar';
+    btnEliminar.addEventListener('click', () => {
+      lista.removeChild(item);
+    });
+
+    // Agregar botones y contenido al li
+    item.appendChild(contenido);
+    item.appendChild(btnEditar);
+    item.appendChild(btnEliminar);
+
+    lista.appendChild(item);
+    formulario.reset();
+  
+});
